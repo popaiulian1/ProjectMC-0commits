08c4a6e770664757ce17c8c10ae6ad23c01aa1e8
George-Cristian Pîrvu <george.pirvuu@gmail.com>
2024-10-30 21:56:51 +0200

    Class Player continue + Class Mage

---
+++ Eter/Eter/Mage.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Eter {
+	enum class MageType {
+		MasterOfFire,
+		MaterOfEarth,
+		MasterOfAir,
+		MasterOfWater
+	};
+
+	class Mage {
+	public:
+		
+	private:
+		MageType m_type;
+	};
+}
--- Eter/Eter/Player.cpp
+++ Eter/Eter/Player.cpp
@@ -1,10 +1,31 @@
 #include "Player.h"
 
-Eter::Player::Player(std::string username) : m_username{ username }
+Eter::Player::Player()
+{
+	m_score = 0;
+}
+
+Eter::Player::Player(std::string username, const int& score, const std::vector<Piece>& cards) : 
+	m_username{ username }, m_score{ score }, m_cards{ cards }
+{
+}
+
+Eter::Player::Player(std::string username, const int& score, const std::vector<Piece>& cards, const Mage& mageCard) :
+	m_username{ username }, m_score{ score }, m_cards{ cards }, m_mageCard{ mageCard }
 {
 }
 
 const std::string& Eter::Player::GetUserName() const
 {
 	return m_username;
+}
+
+const int& Eter::Player::GetScore() const
+{
+	return m_score;
+}
+
+const int& Eter::Player::GetTurnTimeRemaining() const
+{
+	return m_turnTimeRemaining;
 }

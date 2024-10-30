5e961bf982b40b3a2a173df938f05d1d9f0a6270
Man Alexandra <alexandra.man@student.unitbv.ro>
2024-10-30 19:50:34 +0200

    started to implemet powers of cards

--- Eter/Eter/Wizards.cpp
+++ Eter/Eter/Wizards.cpp
@@ -1,14 +1,73 @@
 #include "Wizards.h"
 #include "Board.h"
-
+#include <iostream>
+ 
+//Constructors & Destructor
 Eter::Wizards::Wizards() :
 	name("Unknown"), mageType(MageType::FIRE), magicPowerUsed(false), etherCards(1), board(nullptr) {}
+
 
 Eter::Wizards::Wizards(std::string wizardName, MageType type, const Board* gameBoard) : 
 	name(wizardName), mageType(type), magicPowerUsed(false), etherCards(1), board(gameBoard) {}
 
+
 Eter::Wizards::Wizards(const Wizards& other) :
 	name(other.name), mageType(other.mageType), magicPowerUsed(other.magicPowerUsed), etherCards(other.etherCards), board(other.board) {}
+
+// Determination of Power 
+void Eter::Wizards::fireMasterPower(int powerIndex, int row, int col)
+{
+	if (powerIndex == 1) 
+		eliminateOpponentCard(row, col);
+	
+	else if (powerIndex == 2) 
+		eliminateRow(row);
+	
+}
+
+void Eter::Wizards::earthMasterPower(int powerIndex, int row, int col, std::vector<int>& playerHand)
+{
+	if (powerIndex == 1) 
+		coverOpponentCard();
+	
+	else if (powerIndex == 2) 
+		createPit();
+	
+}
+
+void Eter::Wizards::airMasterPower(int powerIndex, int row, int col)
+{
+	if (powerIndex == 1) 
+		moveOwnStack();
+	
+	else if (powerIndex == 2) 
+		gainExtraEtherCard();
+	
+}
+
+void Eter::Wizards::waterMasterPower(int powerIndex, int srcRow, int srcCol, int destRow)
+{
+	if (powerIndex == 1) 
+		moveOpponentStack();
+	
+	else if (powerIndex == 2) 
+		moveEdgeRow();
+	
+}
+
+// Methods for powers
+/*
+void Eter::Wizards::eliminateOpponentCard(int row, int col)
+{
+	if (board->isOpponentCard(row, col) && board->)
+	{ // We verify if the card at (row, col) is the opponent's card & the card under it is ours
+		board->removeCard(row, col);       // We eliminate the card that is at (row, col) on the board
+		std::cout << "The opponent's card at (" << row << ", " << col << ") has been removed.\n";
+	}
+}
+*/
+
+
 
 
 
--- Eter/Eter/Wizards.h
+++ Eter/Eter/Wizards.h
@@ -20,10 +20,22 @@ namespace Eter {
         Wizards(const Wizards& other);
         ~Wizards()= default;
 
+       // Methods for each MageType, every MageType has two powers, the power will be determined by powerindex
         void fireMasterPower(int powerIndex, int row, int col);
         void earthMasterPower(int powerIndex, int row, int col, std::vector<int>& playerHand);
         void airMasterPower(int powerIndex, int row, int col);
         void waterMasterPower(int powerIndex, int srcRow, int srcCol, int destRow);
+
+        //Methods for Powers determined by powerIndex
+        void eliminateOpponentCard(int row, int col);
+        void eliminateRow(int row);
+        void coverOpponentCard();
+        void createPit();
+        void moveOwnStack();
+        void gainExtraEtherCard();
+        void moveOpponentStack();
+        void moveEdgeRow();
+
 
     private:
         std::string name;

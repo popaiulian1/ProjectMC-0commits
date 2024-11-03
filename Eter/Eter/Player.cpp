75ad199c407d9b39150539bda677638da768018a
George-Cristian Pîrvu <george.pirvuu@gmail.com>
2024-11-03 14:39:12 +0200

    Class Player Updated

--- Eter/Eter/Player.cpp
+++ Eter/Eter/Player.cpp
@@ -1,8 +1,10 @@
 #include "Player.h"
+#include "Piece.h"
 
 Eter::Player::Player()
 {
 	m_score = 0;
+	m_remainingTime = 0;
 }
 
 Eter::Player::Player(std::string username, const int& score, const std::vector<Piece>& cards) : 
@@ -10,8 +12,13 @@ Eter::Player::Player(std::string username, const int& score, const std::vector<P
 {
 }
 
-Eter::Player::Player(std::string username, const int& score, const std::vector<Piece>& cards, const Mage& mageCard) :
-	m_username{ username }, m_score{ score }, m_cards{ cards }, m_mageCard{ mageCard }
+Eter::Player::Player(std::string username, const int& score, const std::vector<Piece>& cards, Wizards wizard) :
+	m_username{ username }, m_score{ score }, m_cards{ cards }, m_wizard { wizard }
+{
+}
+
+Eter::Player::Player(const Player& other) :
+	m_username{ other.m_username }, m_score{ other.m_score }, m_cards{ other.m_cards }
 {
 }
 
@@ -25,7 +32,32 @@ const int& Eter::Player::GetScore() const
 	return m_score;
 }
 
-const int& Eter::Player::GetTurnTimeRemaining() const
+const std::vector<Eter::Piece>& Eter::Player::GetCards() const
 {
-	return m_turnTimeRemaining;
+	return m_cards;
+}
+
+const int& Eter::Player::GetRemainingTime() const
+{
+	return m_remainingTime;
+}
+
+void Eter::Player::setUserName(const std::string& username)
+{
+	m_username = username;
+}
+
+void Eter::Player::setScore(const int& score)
+{
+	m_score = score;
+}
+
+void Eter::Player::setCards(const std::vector<Piece>& cards)
+{
+	m_cards = cards;
+}
+
+void Eter::Player::setRemainingTime(const int& remainingTime)
+{
+	m_remainingTime = remainingTime;
 }
--- Eter/Eter/Player.h
+++ Eter/Eter/Player.h
@@ -2,25 +2,32 @@
 #include <string>
 #include <vector>
 #include "Piece.h"
-#include "Mage.h"
+#include "Wizards.h"
 
 namespace Eter {
 	class Player {
 	public:
 		Player();
 		Player(std::string username, const int& score, const std::vector<Piece>& cards);
-		Player(std::string username, const int& score, const std::vector<Piece>& cards, const Mage& mageCard);
+		Player(std::string username, const int& score, const std::vector<Piece>& cards, Wizards wizard);
+		Player(const Player& other);
 		~Player() = default;
+
 		const std::string& GetUserName() const;
 		const int& GetScore() const;
 		const std::vector<Piece>& GetCards() const;
-		const Mage& GetMageCard() const;
-		const int& GetTurnTimeRemaining() const;
+		const int& GetRemainingTime() const;
+
+		void setUserName(const std::string& username);
+		void setScore(const int& score);
+		void setCards(const std::vector<Piece>& cards);
+		void setRemainingTime(const int& remainingTime);
+
 	private:
 		std::string m_username;
 		int m_score;
 		std::vector<Piece> m_cards;
-		Mage m_mageCard;
-		int m_turnTimeRemaining;
+		Wizards m_wizard;
+		int m_remainingTime;
 	};
 }

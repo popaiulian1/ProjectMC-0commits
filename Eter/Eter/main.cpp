9f2df00a145e9dd431ed64ac6f33af761513d0a6
Popa Marian-Iulian <popaiulian253@gmail.com>
2024-11-29 22:41:29 +0200

    Added Json to project
    added json for a future saving/loading feature for the game. Also added a small test for writing and reading using the json library (check main.cpp for the tests).

--- Eter/Eter/main.cpp
+++ Eter/Eter/main.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
 #include "Game.h"
+#include "json.hpp"
+#include <fstream>
 
+void jsonTestRead() {
+	std::ifstream input("exampleImporting.json");
+	nlohmann::json j;
+	input >> j;
+
+	std::cout << j.dump(4) << std::endl;
+}
+
+void jsonTestWrite() {
+	nlohmann::json j = R"(
+		{
+		"pi": 3.141,
+		"happy": true,
+		"name": "Niels",
+		 "nothing": null,
+		 "answer": {
+		"everything": 42
+		 },
+		 "list": [ 1, 0, 2 ],
+		 "object": {
+		   "currency": "USD",
+		   "value": 42.99
+		 }
+		})"_json;
+
+	std::ofstream output("exampleExporting.json");
+	output << j.dump(4) << std::endl;
+
+}
 
 int main() {
 
-	Eter::Game game;
-	game.StartGame();
+	/*Eter::Game game;
+	game.StartGame();*/
+
+	jsonTestRead();
+	jsonTestWrite();
 
 	return 0;
 }
